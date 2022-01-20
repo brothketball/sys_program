27dee1e65badd3d06ce7b420d60837d493195645
brothketball <563117087@qq.com>
2022-01-20 21:14:44 +0800

    mkdir

---
+++ sys_hello/sys_hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc,char *argv[])
+{
+	//main 输入来自命令行
+	//argc 表示的是命令行中的参数个数
+	//agrv 表示的是命令行中的参数
+	
+	printf("hello world!\n");
+
+	return 0;
+}
