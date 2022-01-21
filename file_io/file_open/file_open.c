4131106533e75c7602ea53ca882f2062fac4d906
brothketball <563117087@qq.com>
2022-01-21 16:07:38 +0800

    establish a dir of file-io

---
+++ file_io/file_open/file_open.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+
+int main(int argc,char *argv[])
+{
+	int fd;
+	fd = open("a.c",O_CREAT|O_RDWR,0666);
+	if(fd < 0)
+	{
+		printf("open is error\n");
+	}
+
+	printf("fd is %d\n",fd);
+
+	return 0;
+}
